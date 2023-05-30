ce150cdb1eb9cfd9f7e985033cd5647892bbcc2f
Alyona K <alengera@gmail.com>
2023-05-30 20:55:10 +0200

    add 9 html and 9 js files

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const ListWithID = document.querySelector("#categories");
+const items = ListWithID.querySelectorAll(".item");
+console.log(`Number of categories: ${items.length}`);
+items.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const elements = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elements}`);
+});
